d09cb029dbf6cc9666038c0eb3ffe9ee6ddef1fa
sotetsuk <koyamada-s@sys.i.kyoto-u.ac.jp>
2016-05-21 19:58:25 +0900

    implement main.py

--- paper2img/main.py
+++ paper2img/main.py
@@ -3,34 +3,55 @@
 """ Convert academic papers (pdf) to nice looking images
 
 Usage:
-  paper2img [--size=<size>] [--num-col] [--num-row] <input-file> <output-file>
+  paper2img stack [--trim=<trim>] [--size=<size>] <input-file> <output-file> <num-col> <num-row>
+  paper2img top [--density=<density>] [--trim=<trim>] [--size=<size>] [--reduce=<reduce>] <input-file> <output-file>
   paper2img (-h | --help)
   paper2img --version
 
 Options:
-  --size=<size>         Set the size (e.g., 100x)
-  --num-col=<num-col>   Set the number of col
-  --num-row=<num-row>   Set the number of row
+  <num-col>             Number of col
+  <num-row>             Number of row
+  --density=<density>   Resolution of image. The higher, the better (default=72)
+  --size=<size>         Size of final output (e.g., 100x)
+  --trim=<trim>         Trimming width and height for each page (e.g., 100x80)
+  --reduce=<reduce>     Determines what % of lower region will be removed (e.g., 50%, default=50%)
   -h --help             Show this screen.
   --version             Show version.
+
+Examples:
+  paper2img stack --trim=100x60 --size=x400 arxiv-paper.pdf out.png
+  paper2img top --size=x400 arxiv-paper.pdf out.png
 """
 
 import os
+import subprocess
 from .manipulator import Manipulator
 from docopt import docopt
 
 
 def main():
+    null = open(os.devnull, 'w')
+    assert subprocess.call(["which", "convert"], stdout=null, stderr=subprocess.STDOUT) == 0, "ImageMagic:convert is required."
+
     args = docopt(__doc__, version='paper2img 0.0.1')
-    print(args)
 
     assert '*' not in args['<input-file>'], "You cannot use *. parse2img should have one input file."
     assert os.path.splitext(args['<input-file>'])[-1] == '.pdf', "Input ext should be .pdf"
     assert os.path.splitext(args['<output-file>'])[-1] == '.png', "Output ext should be .png"
 
-    m = Manipulator(args['input_file'])
-    # some manipulations
-    m.out(args['output_file'])
+    if args['stack']:
+        with Manipulator(args['<input-file>']) as m:
+            m.pdf2png(trim=args["--trim"])
+            m.stack(col=int(args["<num-col>"]), row=int(args['<num-row>']))
+            if args['--size'] is not None:
+                m.resize(args["--size"])
+            m.out(args['<output-file>'])
+
+    if args['top']:
+        with Manipulator(args['<input-file>']) as m:
+            m.pdf2png(trim=args["--trim"], density=args["--density"])
+            m.top() if args['--reduce'] is None else m.top(args["--reduce"])
+            m.out(args['<output-file>'])
 
 
 if __name__ == '__main__':
