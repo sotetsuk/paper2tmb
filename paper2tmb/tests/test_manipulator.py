a05d67452576835a762ace86e1db2c37345176b2
sotetsuk <koyamada-s@sys.i.kyoto-u.ac.jp>
2016-05-21 22:54:12 +0900

    fix .travis.yml

--- paper2tmb/tests/test_manipulator.py
+++ paper2tmb/tests/test_manipulator.py
@@ -18,7 +18,6 @@ class TestManipulator(unittest.TestCase):
             for i in range(12):
                 self.assertTrue(os.path.exists(os.path.join(m.dirname, "pdf2png-{}.png".format(i))))
 
-            m._num_page = 12
             self.assertTrue(m._last == os.path.join(m.dirname, "pdf2png.png"))
 
     def test_pdf2png_trim(self):
@@ -28,7 +27,6 @@ class TestManipulator(unittest.TestCase):
             for i in range(12):
                 self.assertTrue(os.path.exists(os.path.join(m.dirname, "pdf2png-{}.png".format(i))))
 
-            m._num_page = 12
             self.assertTrue(m._last == os.path.join(m.dirname, "pdf2png.png"))
 
     def test_pdf2png_density(self):
@@ -38,17 +36,15 @@ class TestManipulator(unittest.TestCase):
             for i in range(12):
                 self.assertTrue(os.path.exists(os.path.join(m.dirname, "pdf2png-{}.png".format(i))))
 
-            m._num_page = 12
             self.assertTrue(m._last == os.path.join(m.dirname, "pdf2png.png"))
 
     def test_pdf2png_both_trim_density(self):
         with Manipulator("paper2tmb/tests/testdata/1412.6785v2.pdf") as m:
-            m.pdf2png(trim="100x100", density="20")
+            m.pdf2png(trim="300x300", density="10")
 
             for i in range(12):
                 self.assertTrue(os.path.exists(os.path.join(m.dirname, "pdf2png-{}.png".format(i))))
 
-            m._num_page = 12
             self.assertTrue(m._last == os.path.join(m.dirname, "pdf2png.png"))
 
     def test_stack(self):
